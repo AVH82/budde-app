b81b8fe6b7bb20044b01e6b575ddfd7316f57682
AVH82 <adriaan.vanherpen@gmail.com>
2026-07-03 12:26:14 +0200

    Fix OCR validation screen

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'budde-1-0-42';
+const CACHE_NAME = 'budde-1-0-43';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './css/pipboy.css?v=142',
-  './js/app.js?v=142',
+  './css/pipboy.css?v=143',
+  './js/app.js?v=143',
   './js/storage.local.js',
   './js/storage.service.js',
   './js/storage.google-drive.js',
