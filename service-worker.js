2f8fa5906369b345b100bbf92188798f40a9105f
AVH82 <adriaan.vanherpen@gmail.com>
2026-07-05 23:21:28 +0200

    Instrument Buddy Vision debug pipeline (#107)

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = 'budde-3-5-9';
+const CACHE_NAME = 'budde-3-5-10';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './css/pipboy.css?v=359',
-  './js/app.js?v=359',
-  './js/buddy.js?v=359',
+  './css/pipboy.css?v=3510',
+  './js/app.js?v=3510',
+  './js/buddy.js?v=3510',
   './js/storage.local.js',
   './js/storage.service.js',
   './js/storage.google-drive.js',
   './js/google-auth.service.js',
-  './js/buddy-vision.service.js?v=359',
+  './js/buddy-vision.service.js?v=3510',
   './js/receipt-ocr.service.js',
   './assets/logo/budde_logo.png',
   './assets/nav/home.png',
