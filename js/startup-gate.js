3ef0f77f739d2fdb84135f84fbbcc72c4fd86620
AVH82 <adriaan.vanherpen@gmail.com>
2026-07-15 16:53:05 +0200

    AST-022 restore header text centering

--- js/startup-gate.js
+++ js/startup-gate.js
@@ -1,7 +1,7 @@
 (function(){
   const FRAME_STYLESHEET='css/frame-system-v2.css?v=ast0117';
   const RELEASE_STYLESHEET='css/ast-012-4.css?v=ast0124';
-  const HEADER_STYLESHEET='css/ast-013-2.css?v=ast021-transparent';
+  const HEADER_STYLESHEET='css/ast-013-2.css?v=ast022-center';
   const FRAME_MOTION_MS=2600;
   let awaitingGoogleAuth=false;
 
